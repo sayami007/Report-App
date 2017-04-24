b34aec8a419326007e5e3d525b6aa47653b84a30
Bibesh Manandhar <bibesh.manandhar@gmail.com>
2017-04-24 17:42:37 +0545

    -- Events with calendar

--- app/src/main/java/mic/unlimited/com/reportingapp/Events/Events.java
+++ app/src/main/java/mic/unlimited/com/reportingapp/Events/Events.java
@@ -6,7 +6,7 @@ import org.androidannotations.annotations.App;
 import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EActivity;
 
-import mic.unlimited.com.reportingapp.Events.Question.Event1Q_;
+import mic.unlimited.com.reportingapp.Events.Question.*;
 import mic.unlimited.com.reportingapp.R;
 
 /**
@@ -15,8 +15,60 @@ import mic.unlimited.com.reportingapp.R;
 
 @EActivity(R.layout.activity_events)
 public class Events extends AppCompatActivity {
+
     @Click(R.id.event1)
     void event1() {
         Event1Q_.intent(this).start();
     }
+
+
+    @Click(R.id.event2)
+    void event2() {
+        Event2Q_.intent(this).start();
+    }
+
+
+    @Click(R.id.event3)
+    void event3() {
+        Event3Q_.intent(this).start();
+    }
+
+
+    @Click(R.id.event4)
+    void event4() {
+        Event4Q_.intent(this).start();
+    }
+
+
+    @Click(R.id.event5)
+    void event5() {
+        Event5Q_.intent(this).start();
+    }
+
+
+    @Click(R.id.event6)
+    void event6() {
+        Event6Q_.intent(this).start();
+    }
+
+
+    @Click(R.id.event7)
+    void event7() {
+        Event7Q_.intent(this).start();
+    }
+
+
+    @Click(R.id.event9)
+    void event9() {
+        Event9Q_.intent(this).start();
+    }
+
+
+    @Click(R.id.event10)
+    void event10() {
+        Event10Q_.intent(this).start();
+    }
+
+
+
 }
--- app/src/main/java/mic/unlimited/com/reportingapp/Events/Question/Event1Q.java
+++ app/src/main/java/mic/unlimited/com/reportingapp/Events/Question/Event1Q.java
@@ -1,8 +1,18 @@
 package mic.unlimited.com.reportingapp.Events.Question;
 
+import android.app.Dialog;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
 
 import mic.unlimited.com.reportingapp.R;
 
@@ -12,4 +22,27 @@ import mic.unlimited.com.reportingapp.R;
 
 @EActivity(R.layout.activity_q_event1)
 public class Event1Q extends AppCompatActivity {
+
+    String mainDate = null;
+
+    @ViewById
+    TextView selectedDate;
+
+    @Click(R.id.pickDate)
+    void openDateTimePicker() {
+        final Dialog dateTimePicker = new Dialog(this);
+        dateTimePicker.setContentView(R.layout.datetime_pick);
+        final DatePicker date = (DatePicker) dateTimePicker.findViewById(R.id.calendar);
+        Button b1 = (Button) dateTimePicker.findViewById(R.id.pickDate2);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mainDate = date.getYear() + "/" + date.getMonth() + "/" + date.getDayOfMonth();
+                selectedDate.setText(mainDate);
+                dateTimePicker.dismiss();
+            }
+        });
+        dateTimePicker.show();
+
+    }
 }
